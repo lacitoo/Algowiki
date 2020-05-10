c4a6a5070a239e121c5b9fd849987c66886c28ba
bahramib <61052054+bahramib@users.noreply.github.com>
2020-05-10 07:04:45 +0200

    Add files via upload

--- Szeminarium_2020_tavasz/Bahrami_Benedek_Attila/majus/19 B_Checkout.cpp
+++ Szeminarium_2020_tavasz/Bahrami_Benedek_Attila/majus/19 B_Checkout.cpp
@@ -4,7 +4,7 @@
  
 int n;
  
-long long find_lowest(int ind, int skips, std::pair<int, int>* arr, long long** dp)
+long long find_lowest(int ind, int skips, std::pair<int, int>* pairs, long long** dp)
 {
 	if (ind == n)
 		if (skips < 0)
@@ -12,9 +12,9 @@ long long find_lowest(int ind, int skips, std::pair<int, int>* arr, long long** 
 		else
 			return 0;
 	long long& ret = dp[ind][skips + n];
-	if (~ret)
+	if (ret != -1)
 		return ret;
-	return ret = std::min(find_lowest(ind + 1, std::min(skips + arr[ind].second, n), arr, dp) + arr[ind].first, find_lowest(ind + 1, skips - 1, arr, dp));
+	return ret = std::min(find_lowest(ind + 1, std::min(skips + pairs[ind].second, n), pairs, dp) + pairs[ind].first, find_lowest(ind + 1, skips - 1, pairs, dp));
 }
  
 int main()
@@ -29,12 +29,19 @@ int main()
 			dp[i][k] = -1;
 		}
 	}
-	std::pair<int, int>* arr = new std::pair<int, int> [n];
+	std::pair<int, int>* pairs = new std::pair<int, int> [n];
 	for (int i = 0; i < n; i++)
 	{
 		int c, t;
 		std::cin >> t >> c;
-		arr[i] = { c, t };
+		pairs[i] = { c, t };
 	}
-	std::cout << find_lowest(0, 0, arr, dp);
+	std::cout << find_lowest(0, 0, pairs, dp);
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] dp[i];
+	}
+	delete[] dp;
+	delete[] pairs;
 }
